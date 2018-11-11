2f341f57de82329b511da80bae0f814510576f13
Ye-Fee Liang <yf.liang@yahoo.ca>
2018-11-10 23:25:05 -0800

    initial commit.

---
+++ arith.py
@@ -0,0 +1,8 @@
+def dbl(val):
+    result = val * 2
+    return result
+
+def addem(a, b):
+    result = a + b
+    return result
+
---
+++ test_soup.dir/urllinks.py
@@ -0,0 +1,17 @@
+# Note - this code must run in Python 2.x and you must download
+# http://www.pythonlearn.com/code/BeautifulSoup.py
+# Into the same folder as this program
+# source:        http://www.crummy.com/software/BeautifulSoup/
+# sample codes:  http://www.pythonlearn.com/code/
+
+import urllib
+from BeautifulSoup import *
+
+url = raw_input('Enter - ')
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print tag.get('href', None)
